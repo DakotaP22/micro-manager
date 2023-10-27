0e5939fc2d426e4a67a35181a8961d16e06b55fb
Dakota Pease <peased22@gmail.com>
2023-10-27 19:44:10 -0400

    Revert "feat(layout): add sidenav layout"
    This reverts commit 6d48e252708697f85a363d92147775e31ae76d32.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatSidenavModule],
+  imports: [RouterModule],
   selector: 'micro-manager-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
