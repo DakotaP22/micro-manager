1ebff77747c2b106becbe19807dfd89ff74bcc7b
Dakota Pease <peased22@gmail.com>
2023-10-29 00:33:46 -0400

    feat(landing): add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '', 
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('@micromanager/landing').then(m => m.HomeComponent)
+    }
+];
