43de8cd25b08160754f07b2efceb477efe6b5ea8
Dakota Pease <peased22@gmail.com>
2023-11-11 22:23:15 -0500

    fix(firebase): point emulator to proper project id

--- environments/src/lib/environment.ts
+++ environments/src/lib/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   useEmulators: true,
   firebase_config: {
-    projectId: 'demo-project',
+    projectId: 'demo-micro-manager-dp01',
     appId: '1:240698918201:web:da53c14f381c8b10ea14e8',
     storageBucket: 'micro-manager-dp01.appspot.com',
     apiKey: 'AIzaSyBL61WG1EfjswoRi40lVVku6DNLhU_jJXA',
