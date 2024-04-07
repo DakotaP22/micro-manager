88439432ce3b331ae49ba6cfdbd9cafb272b1d63
Dakota Pease <peased22@gmail.com>
2024-04-07 00:52:28 -0400

    feat: adds sidenav layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule],
+  imports: [RouterOutlet, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/features/workbuckets/pages/workbucket-page/workbucket-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workbucket-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './workbucket-page.component.html',
+  styleUrl: './workbucket-page.component.scss'
+})
+export class WorkbucketPageComponent {
+
+}
