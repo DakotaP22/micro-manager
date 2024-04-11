671b4c619be5bf9e1626bb81fce6873e4b959179
Dakota Pease <peased22@gmail.com>
2024-04-10 23:39:00 -0400

    fix(workbuckets): 00:00 AM now shows as 12:00 AM on meetings-overview-card component

--- src/app/features/workbuckets/pages/workbucket-page/components/meetings-overview-card/meetings-overview-card.component.ts
+++ src/app/features/workbuckets/pages/workbucket-page/components/meetings-overview-card/meetings-overview-card.component.ts
@@ -25,17 +25,17 @@ export class MeetingsOverviewCardComponent {
     const d1Date = `${date1.getMonth()}/${date1.getDate()}/${date1.getFullYear()}`;
     const d1Time = `${
       date1.getHours() > 12
-        ? this.format(date1.getHours() - 12)
-        : this.format(date1.getHours())
-    }:${this.format(date1.getMinutes())}`;
+        ? this.formatHour(date1.getHours() - 12)
+        : this.formatHour(date1.getHours())
+    }:${this.formatMinute(date1.getMinutes())}`;
     const d1TOD = `${date1.getHours() > 12 ? 'PM' : 'AM'}`;
 
     const d2Date = `${date2.getMonth()}/${date2.getDate()}/${date2.getFullYear()}`;
     const d2Time = `${
       date2.getHours() > 12
-        ? this.format(date2.getHours() - 12)
-        : this.format(date2.getHours())
-    }:${this.format(date2.getMinutes())}`;
+        ? this.formatHour(date2.getHours() - 12)
+        : this.formatHour(date2.getHours())
+    }:${this.formatMinute(date2.getMinutes())}`;
     const d2TOD = `${date2.getHours() > 12 ? 'PM' : 'AM'}`;
 
     if (date1.getDate() === date2.getDate()) {
@@ -45,7 +45,12 @@ export class MeetingsOverviewCardComponent {
     }
   });
 
-  private format(num: number): string {
+  private formatHour(num: number): string {
+    const formatted = num < 10 ? `0${num}` : `${num}`;
+    return formatted === '00' ? '12' : formatted;
+  }
+
+  private formatMinute(num: number): string {
     return num < 10 ? `0${num}` : `${num}`;
   }
 }
