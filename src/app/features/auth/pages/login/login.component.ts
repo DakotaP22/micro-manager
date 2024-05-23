cd48a6d253095c2e050b381942c0f67abe57eb94
Dakota Pease <peased22@gmail.com>
2024-05-22 23:01:41 -0400

    feat(auth): connect login page to auth service

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
@@ -14,9 +16,27 @@ import { RouterModule } from '@angular/router';
 })
 export class LoginComponent {
 
+  private authSvc = inject(AuthService);
+
+  user = toSignal(this.authSvc.user$);
+
   loginForm = inject(FormBuilder).group({
     email: [''],
     password: ['']
   })
+
+  constructor() {
+    effect(() => {
+      console.log('User:', this.user());
+    });
+  }
+
+
+  signIn() {
+    const { email, password } = this.loginForm.value;
+    if(!!email && !!password) {
+      this.authSvc.signIn(email, password);
+    }
+  }
 
 }
