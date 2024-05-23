90e0aee98db09079eb8a18665639128a6f9956b7
Dakota Pease <peased22@gmail.com>
2024-05-22 22:41:33 -0400

    feat: add initial auth service

---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { Auth, UserCredential, idToken, signInWithEmailAndPassword, signOut, user } from '@angular/fire/auth';
+import { map } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  private auth = inject(Auth);
+  user$ = user(this.auth);
+  idToken$ = idToken(this.auth);
+  isAuthenticated$ = this.idToken$.pipe(map(token => !!token));
+
+  signIn(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  signOut(): Promise<void> {
+    return signOut(this.auth);
+  }
+
+
+}
