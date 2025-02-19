d0cddaf68a7a5f2b6fcd62753f052feb135ebe90
Dakota Pease <peased22@gmail.com>
2025-02-19 17:12:44 +0000

    feat(workbucket): Adds a button and output to the workbucket-dropdown for triggering the creation of a new workbucket

--- src/app/feature/workbucket/components/workbucket-dropdown/workbucket-dropdown.component.ts
+++ src/app/feature/workbucket/components/workbucket-dropdown/workbucket-dropdown.component.ts
@@ -1,27 +1,76 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, effect, input, output, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectModule, MatSelect } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 import { Workbucket } from '../../models/Workbucket';
 
 @Component({
   selector: 'workbucket-dropdown',
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, MatIconModule],
+  styles: `
+    mat-form-field { width: 100%; }
+    mat-icon { margin-inline-end: 8px; }
+
+    button {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+      height: 48px;
+      display: flex;
+      align-items: center;
+      text-align: left;
+      padding: 0 16px;
+      font-family: Roboto;
+      font-size: 16px;
+      font-weight: 500;
+    }
+
+    button:hover {
+      background-color: var(--mat-option-hover-state-layer-color,
+        color-mix(
+          in srgb,
+          var(--mat-sys-on-surface) calc(var(--mat-sys-hover-state-layer-opacity)* 100%),
+          transparent
+        )
+      );
+    }
+  `,
   template: `
     <mat-form-field>
-      <mat-select [value]="selectedWorkbucketId()" (selectionChange)="workbucketSelect.emit($event.value)">
+      <mat-select
+        #select
+        [value]="selectedWorkbucketId()"
+        (selectionChange)="workbucketSelect.emit($event.value)"
+      >
+        <button (click)="onNewWorkbucketClick()">
+          <mat-icon fontIcon="add_circle_outline" />
+          <span>Add New Bucket</span>
+        </button>
         @for (workbucket of workbuckets(); track workbucket.id) {
-          <mat-option [value]="workbucket.id">{{ workbucket.name }}</mat-option>
+        <mat-option [value]="workbucket.id">{{ workbucket.name }}</mat-option>
         }
       </mat-select>
     </mat-form-field>
   `,
-  styles: `
-    mat-form-field { width: 100%; }
-  `,
 })
 export class WorkbucketDropdownComponent {
+  matSelect = viewChild.required(MatSelect);
+
   workbuckets = input.required<Workbucket[]>();
   selectedWorkbucketId = input.required<string>();
   workbucketSelect = output<string>();
+  newWorkbucketTrigger = output<void>();
+
+  onSelectionChange(id: string) {
+    if (id) {
+      this.workbucketSelect.emit(id);
+    }
+  }
+
+  onNewWorkbucketClick() {
+    this.matSelect().close();
+    this.newWorkbucketTrigger.emit();
+  }
 }
