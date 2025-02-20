c2e5b91c455f4144df9a11d6471ea5c7161d8811
Dakota Pease <peased22@gmail.com>
2025-02-20 12:23:58 +0000

    feat(workbucket): Adds addWorkItem mutator to work-item resource function

--- src/app/feature/workbucket/utils/work-item.resource.ts
+++ src/app/feature/workbucket/utils/work-item.resource.ts
@@ -1,33 +1,66 @@
-import { assertInInjectionContext, inject, Signal } from "@angular/core";
-import { rxResource } from "@angular/core/rxjs-interop";
-import { collection, collectionData, Firestore } from "@angular/fire/firestore";
-import { Observable, of } from "rxjs";
-import { WorkItem } from "../models/WorkItem";
+import { assertInInjectionContext, inject, Signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  Firestore,
+} from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { WorkItem, WorkItemFirebaseDto } from '../models/WorkItem';
 
 export function injectWorkItemsResource(
-    userIdSignal: Signal<string | undefined>,
-    workbucketIdSignal: Signal<string | undefined>
+  userIdSignal: Signal<string | undefined>,
+  workbucketIdSignal: Signal<string | undefined>
 ) {
-    assertInInjectionContext(injectWorkItemsResource);
-    const firestore = inject(Firestore);
+  assertInInjectionContext(injectWorkItemsResource);
+  const firestore = inject(Firestore);
 
-    const resource = rxResource({
-        request: () => ({
-            userId: userIdSignal(),
-            workbucketId: workbucketIdSignal()
-        }),
-        loader: ({ request }) => {
-            const { userId, workbucketId } = request;
+  const resource = rxResource({
+    request: () => ({
+      userId: userIdSignal(),
+      workbucketId: workbucketIdSignal(),
+    }),
+    loader: ({ request }) => {
+      const { userId, workbucketId } = request;
 
-            if (!userId || !workbucketId) return of([]);
+      if (!userId || !workbucketId) return of([]);
 
-            const workItemCollection = collection(firestore, 'USER_DATA', userId, 'WORKBUCKETS', workbucketId, 'WORK_ITEMS');
-            return collectionData(workItemCollection, { idField: 'id' }) as Observable<WorkItem[]>;
-        }
-    })
+      const workItemCollection = collection(
+        firestore,
+        'USER_DATA',
+        userId,
+        'WORKBUCKETS',
+        workbucketId,
+        'WORK_ITEMS'
+      );
+      return collectionData(workItemCollection, {
+        idField: 'id',
+      }) as Observable<WorkItem[]>;
+    },
+  });
 
+  return {
+    resource,
+    addWorkItem: async (workItem: WorkItemFirebaseDto) => {
+      const userId = userIdSignal();
+      const workbucketId = workbucketIdSignal();
 
-    return {
-        resource,
-    }
+      if (!userId || !workbucketId) return;
+
+      const workItemCollection = collection(
+        firestore,
+        'USER_DATA',
+        userId,
+        'WORKBUCKETS',
+        workbucketId,
+        'WORK_ITEMS'
+      );
+
+      const doc = await addDoc(workItemCollection, workItem);
+
+      resource.reload();
+      return doc.id;
+      },
+  };
 }
